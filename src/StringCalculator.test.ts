00ed75d95afea5d00ef4efcde91f133bfcdbccdb
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2024-08-07 10:29:31 +0530

    Added Step 5 functionaity and test

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -26,5 +26,12 @@ describe('StringCalculator', () => {
     expect(calculator.Add("1,2,3,4,5")).toBe(15);
   });
 
+  //5th Step Test
+  it('should handle new lines between numbers', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.Add("1\n2,3")).toBe(6);
+  });
+  
+
 });
 
--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,7 +1,7 @@
 export class StringCalculator {
     public Add(numbers: string): number {
       if (numbers === "") return 0;
-      const numberArray = numbers.split(",").map(Number);
+      const numberArray = numbers.split(/,|\n/).map(Number);
       return numberArray.reduce((sum, num) => sum + num, 0);
     }
   }
